b8cad6fdc9d01d152c0deb2b9dcacfb2040c265f
LuisCif-20 <n.rodas225@gmail.com>
2021-06-18 14:09:02 -0600

    Juego Damas beta

--- src/juego/Principal.java
+++ src/juego/Principal.java
@@ -1,23 +1,51 @@
 package src.juego;
 
+import java.util.*;
 import src.jugadores.VectorJugador;
 import src.tablero.Tablero;
 
 public class Principal {
 
     public static void main(String[] args) {
-        Principal principal = new Principal()
-;    }
+        Principal principal = new Principal();
+    }
 
+    Scanner scanner = new Scanner(System.in);
     private VectorJugador jugadores = new VectorJugador();
     private Tablero tablero = new Tablero();
 
-    public Principal(){
-        System.out.println("\nDamas Chinas\n");
-        jugadores.escogerPrimerJugador();
-        jugadores.escogerSegundoJugador();
-        System.out.println("");
-        tablero.pintarTablero();
+    public Principal() {
+        int menu;
+        do {
+            System.out.println("\nDamas Chinas\n");
+            System.out.println("¿Qué deseas hacer");
+            System.out.println("1) Jugar");
+            System.out.println("2) Ver Tablero");
+            System.out.println("3) Ver posicion de fichas");
+            System.out.println("4) Mostrar Jugadores");
+            System.out.println("5) Mostrar visualizacion de Partida");
+            System.out.println("6) Reportes");
+            System.out.println("7) Salir\n");
+            menu = Integer.parseInt(scanner.nextLine());
+            if (menu == 1) {
+                System.out.println("Aun no esta disponible esta opcion");
+            } else if (menu == 2) {
+                tablero.pintarTablero();
+            } else if (menu == 3) {
+                tablero.pintarFichasTablero();
+            } else if (menu == 4) {
+                jugadores.mostrarJugadores();
+            } else if (menu == 5) {
+                jugadores.escogerPrimerJugador();
+                jugadores.escogerSegundoJugador();
+                tablero.pintarFichasTablero();
+            } else if (menu == 6) {
+                System.out.println("Esta opcion aun no esta lista");
+            } else {
+                System.out.println("Hasta Pronto");
+            }
+        } while (menu >= 1 & menu <= 6);
+
     }
 
 }
--- src/tablero/Tablero.java
+++ src/tablero/Tablero.java
@@ -20,18 +20,72 @@ public class Tablero {
                     if (par1 != 0) {
                         System.out.print("\u001B[47m" +"    " + "\u001B[0m");
                     } else {
-                        System.out.print("    ");
+                        System.out.print("\u001B[41m"+"    " + "\u001B[0m");
                     }
                 } else {
                     if (par1 == 0) {
                         System.out.print("\u001B[47m" +"    " + "\u001B[0m");
                     } else {
-                        System.out.print("    ");
+                        System.out.print("\u001B[41m"+"    " + "\u001B[0m");
                     }
                 }
             }
             System.out.println("");
         }
     }
+
+    public void pintarFichasTablero(){
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[i].length; j++) {
+                int par1 = j%2;
+                int par2 = i%2;
+                if (i < 2) {
+                    if (par2 == 0) {
+                        if (par1 != 0) {
+                            System.out.print("\u001B[47m" +"    " + "\u001B[0m");
+                        } else {
+                            System.out.print("\u001B[41m" + "\u001B[37m" + "  * " + "\u001B[0m");
+                        }
+                    } else {
+                        if (par1 == 0) {
+                            System.out.print("\u001B[47m" +"    " + "\u001B[0m");
+                        } else {
+                            System.out.print("\u001B[41m" + "\u001B[37m" + "  * " + "\u001B[0m");
+                        }
+                    }
+                } else if (i > 5) {
+                    if (par2 == 0) {
+                        if (par1 != 0) {
+                            System.out.print("\u001B[47m" +"    " + "\u001B[0m");
+                        } else {
+                            System.out.print("\u001B[41m" + "\u001B[30m" + "  * " + "\u001B[0m");
+                        }
+                    } else {
+                        if (par1 == 0) {
+                            System.out.print("\u001B[47m" +"    " + "\u001B[0m");
+                        } else {
+                            System.out.print("\u001B[41m" + "\u001B[30m" + "  * " + "\u001B[0m");
+                        }
+                    }
+                } else {
+                    if (par2 == 0) {
+                        if (par1 != 0) {
+                            System.out.print("\u001B[47m" +"    " + "\u001B[0m");
+                        } else {
+                            System.out.print("\u001B[41m" + "\u001B[30m" + "    " + "\u001B[0m");
+                        }
+                    } else {
+                        if (par1 == 0) {
+                            System.out.print("\u001B[47m" +"    " + "\u001B[0m");
+                        } else {
+                            System.out.print("\u001B[41m" + "\u001B[30m" + "    " + "\u001B[0m");
+                        }
+                    } 
+                }
+            }
+            System.out.println("");
+        }
+    }
+
     //Getter y Setter
 }
